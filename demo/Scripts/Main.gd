283b9c2a0426294bb7c5d19cf7f10adf0c70c29a
Alex <alexpfonseka@gmail.com>
2019-06-26 14:38:31 +1000

    Update demo project

--- demo/Scripts/Main.gd
+++ demo/Scripts/Main.gd
@@ -1,19 +1,10 @@
 extends Node2D
 
 func _ready():
-	# set up FMOD 
-	FMOD.system_set_software_format(0, Fmod.FMOD_SPEAKERMODE_STEREO, 0)
-	FMOD.system_init(1024, Fmod.FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.FMOD_INIT_NORMAL)
-	
-	# load banks
-	FMOD.bank_load("./Banks/Desktop/Master Bank.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
-	FMOD.bank_load("./Banks/Desktop/Master Bank.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
-	
 	# register listener
-	FMOD.system_add_listener($Listener)
+	Fmod.system_add_listener($Listener)
 	
 	# play some events
 	# technically these are not one-shots but this is just for demo's sake
-	FMOD.play_one_shot("event:/Car engine", $SoundSource1)
-	FMOD.play_one_shot("event:/Waterfall", $SoundSource2)
-			
+	Fmod.play_one_shot("event:/Car engine", $SoundSource1)
+	Fmod.play_one_shot("event:/Waterfall", $SoundSource2)
