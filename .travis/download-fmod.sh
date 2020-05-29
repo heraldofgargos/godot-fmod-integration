d8b3cd1e8a08b4dedcfe4e5241d8fb3946dfe748
Alex <AlexPFonseka@gmail.com>
2020-05-29 10:44:12 +1000

    [Travis] Fix build for iOS and macOS

--- .travis/download-fmod.sh
+++ .travis/download-fmod.sh
@@ -7,7 +7,7 @@ fi
 if [[ $3 == "osx" ]]
 then
   URL=$(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" https://www.fmod.com/api-get-download-link\?path\=files/fmodstudio/api/Mac/\&filename\=fmodstudioapi20006mac-installer.dmg\&user\=$1 | jq -r '.url')
-  wget -O fmodstudioapi20006osx.dmg $URL
+  wget -O fmodstudioapi20006mac-installer.dmg $URL
 fi
 if [[ $3 == "windows" ]]
 then
@@ -17,7 +17,7 @@ fi
 if [[ $3 == "iphone" ]]
 then
   URL=$(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" https://www.fmod.com/api-get-download-link\?path\=files/fmodstudio/api/iOS/\&filename\=fmodstudioapi20006ios-installer.dmg\&user\=$1 | jq -r '.url')
-  wget -O fmodstudioapi20006ios.dmg $URL
+  wget -O fmodstudioapi20006ios-installer.dmg $URL
 fi
 if [[ $3 == "android" ]]
 then
